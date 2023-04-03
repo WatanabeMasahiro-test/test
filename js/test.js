2ac1416dec3491a45b35a8e9ac898a2b0e284f68
masahirowatanabe <2c.nave.stk@gmail.com>
2023-04-03 12:22:38 +0900

    hogehoge

---
+++ js/test.js
@@ -0,0 +1,9 @@
+function process() {
+	console.log("HELLO");
+	document.getElementById('target').textContent = '文字がわり';
+	document.getElementById('trigger_img').classList.add('changed');
+}
+
+document.getElementById('trigger_img').addEventListener('click', process);
+
+alert("test");
